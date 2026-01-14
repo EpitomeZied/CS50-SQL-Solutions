e6cdad4ad21fb029c67eefd1e272b0d4ccccddd1
Mahmoud Abo-Zied <mahmoudkailad4444@gmail.com>
2026-01-14 11:49:59 +0200

    feat: Add meteorite data import SQL and CSV file
    Introduce a new SQL script for importing meteorite data from a CSV file. The script creates a "meteorites" table, processes the data by cleaning and transforming it, and populates the table with the cleaned data. This commit enhances the database with comprehensive meteorite information for further analysis.

---
+++ Week 3 Writing/Meteorite Cleaning/import.sql
@@ -0,0 +1,44 @@
+.import --csv meteorites.csv "meteorites_temp"
+
+CREATE TABLE "meteorites"
+(
+    "id"        INTEGER,
+    "name"      TEXT NOT NULL,
+    "class"     TEXT NOT NULL,
+    "mass"      NUMBER,
+    "discovery" TEXT NOT NULL CHECK ("discovery" IN ('Fell', 'Found')),
+    "year"      INTEGER,
+    "lat"       NUMBER,
+    "long"      NUMBER,
+    PRIMARY KEY ("id")
+);
+
+DELETE
+FROM "meteorites_temp"
+WHERE "nametype" = 'Relict';
+
+UPDATE "meteorites_temp"
+SET "mass" = NULL
+WHERE "mass" = '';
+UPDATE "meteorites_temp"
+SET "year" = NULL
+WHERE "year" = '';
+UPDATE "meteorites_temp"
+SET "lat" = NULL
+WHERE "lat" = '';
+UPDATE "meteorites_temp"
+SET "long" = NULL
+WHERE "long" = '';
+
+UPDATE "meteorites_temp"
+SET "mass" = ROUND("mass", 2);
+UPDATE "meteorites_temp"
+SET "lat" = ROUND("lat", 2);
+UPDATE "meteorites_temp"
+SET "long" = ROUND("long", 2);
+INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
+SELECT "name", "class", "mass", "discovery", "year", "lat", "long"
+FROM "meteorites_temp"
+ORDER BY "year", "name";
+
+DROP TABLE "meteorites_temp";
